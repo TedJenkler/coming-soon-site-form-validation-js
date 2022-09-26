c567520f974626f0fd467a30af8ad8f89eed0566
Teodor Jenkler <108183860+TedJenkler@users.noreply.github.com>
2022-09-26 14:10:14 +0200

    bug fix

--- index.js
+++ index.js
@@ -5,17 +5,19 @@ let email = document.querySelector('#email')
 let form = document.querySelector('#form')
 
 function validateEmail(email) {
-    const res = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
     return res.test(String(email).toLowerCase());
 }
 
 form.addEventListener('submit', (e) => {
 
-    if(email.value === ""){
+    const res = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if(email.value === "" || !email.value.match(res)){
     e.preventDefault()
-    errorText.classList.toggle("errortext")
-    errorAlert.classList.toggle("erroralert")
-    errorInput.classList.toggle("errorinput")
+    errorText.classList.remove("errortext")
+    errorAlert.classList.remove("erroralert")
+    errorInput.classList.remove("errorinput")
     }
     else {
 
